65066c5bf08d81dcc97cbe108773256a7f5db984
Rinu Zachariah <Devisha Modi-TCS@MCKINSEY.COM>
2016-09-13 17:49:05 +0530

    API check failed

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-/* eslint-disable no-console */
-console.log('hi');
+import ReactDOM from 'react-dom';
+import React from 'react';
+import $ from 'jquery';
+var result = $.ajax({
+				url: 'http://api.openaura.com/v1/search/artists?q=taylor&api_key=ae8bbaabb8e2a9f0ba172c0414cf99389beabc11', 
+				data: null,
+				type: 'GET',
+				datatype: 'JSONP',
+				// headers: {"Access-Control-Allow-Origin": "http://localhost:4000", 
+				// 			"Access-Control-Allow-Credentials": true},
+
+				//'Access-Control-Allow-Origin': 'http://localhost:4000',
+				success: function(data){
+					console.log(data);
+				},
+				error: function(data){
+					alert('error');
+				}
+			});
+
+// React.createClass({
+// 	render :
+// 		(
+			
+// 		)
+// })
+ReactDOM.render(<h1>Hello</h1>, document.getElementById('app'));
+
+	
