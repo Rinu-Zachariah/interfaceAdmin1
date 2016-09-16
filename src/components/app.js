44496c0e65f700fdbc5d231e99bbb0c1a15a367d
Xavier Martin <Xavier Martin@MCKINSEY.COM>
2016-09-16 13:39:21 +0530

    Rectified Routing Issue & added a basic UI structure

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,7 @@ class App extends Component{
         <div style={{float: "left", marginTop: "100px"}}>
           <Menu />
         </div>
-        <div style={{textAlign: "center", margin: "5% 12% 0% 150px", border: "lightgray 1px solid", minHeight: "400px"}}>
+        <div style={{textAlign: "center", margin: "5% 12% 0% 150px", border: "lightgray 1px solid", minHeight: "400px", boxShadow: "5px 5px 5px 5px #c7c7c7"}}>
           {this.props.children}
         </div>
       </div>
