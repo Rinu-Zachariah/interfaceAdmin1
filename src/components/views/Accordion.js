906b5e5d990278154fad44ebbbf545a530de2549
Minal Shettigar <minal.shettigar@tcs.com>
2016-10-20 20:24:46 +0530

    Poll delete functionality added

--- src/components/views/Accordion.js
+++ src/components/views/Accordion.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import $ from 'jquery';
 import * as PollActions from '../../actions/PollActions';
 
@@ -44,16 +45,16 @@ class Accordion extends React.Component {
   const stateStyle = this.state.active ? styles.active : styles.inactive;
   return (
         <section className="accordionContainer">
-        <div className="heading">
-          <a onClick={this.toggle}>
-          {this.props.year}
-        </a>
-        </div>
-        <div style={stateStyle} className="mainContent">
-          <p dangerouslySetInnerHTML={{__html: this.props.summary}} />
-          <ul>{this.props.data}</ul>
-          <button className="btn btn-danger pull-right" onClick={()=>{this.onDelete(this.props.event1);}} value="delete">Remove</button>
-          <button className="btn btn-warning pull-right">Edit</button>
+          <div className="heading">
+            <a onClick={this.toggle}>
+              {this.props.year}
+            </a>
+          </div>
+          <div style={stateStyle} className="mainContent">
+            <p dangerouslySetInnerHTML={{__html: this.props.summary}} />
+            <ul>{this.props.data}</ul>
+            <button className="btn btn-danger pull-right" onClick={()=>{this.onDelete(this.props.event1);}} value="delete">Remove</button>
+            <button className="btn btn-warning pull-right">Edit</button>
           </div>
         </section>
       );
@@ -68,4 +69,12 @@ Accordion.propTypes ={
   event1:React.PropTypes.object
 };
 
-export default Accordion;
+function mapStateToProps(state,ownProps){
+  console.log("inside accordion page");
+  console.log(state.poll);
+  return {
+    poll:state.poll
+  };
+}
+
+export default connect(mapStateToProps)(Accordion);
